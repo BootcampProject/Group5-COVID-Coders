a6d5e8ce00157ac009786873e65e575680668e12
Lola <lia1m@aol.com>
2021-07-27 17:24:04 -0400

    started to create CSS and html boilerplate with Uikit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
-$(document).ready(function (){
-
+document.ready(function(){
+  var urlCovid = "http://api.springernature.com/metadata/pam/doi/10.1007/s11276-008-0131-4?api_key=fbae3d20fbfb6ed713ee5317f945feeaf20426dc"
+    console.log(urlCovid)
 });
